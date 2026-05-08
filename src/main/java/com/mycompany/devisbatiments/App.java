affb96d789b1703b524e32a77aa57f9f5199686a
delph <delph@192.168.0.23>
2026-05-08 21:22:03 +0200

    Premier

--- src/main/java/com/mycompany/devisbatiments/App.java
+++ src/main/java/com/mycompany/devisbatiments/App.java
@@ -2,6 +2,7 @@ package com.mycompany.devisbatiments;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -16,11 +17,17 @@ public class App extends Application {
     public void start(Stage stage) {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
+        Button bouton = new Button("Clique!");
+        bouton.setOnAction(e -> {
+        System.out.println("Bouton cliqué !");
+        });
+        StackPane root = new StackPane();
+        root.getChildren().add(bouton);
+        Scene scene = new Scene(root, 300, 200);
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
         stage.show();
+        
     }
 
     public static void main(String[] args) {
